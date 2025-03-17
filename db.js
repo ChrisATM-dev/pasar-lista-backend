5e3cb218347f4a9ff8dace9522829a1f4e883c35
Christian Torres <Christian.atm24@gmail.com>
2025-03-17 17:27:25 -0300

    problemas con la coneccion

--- db.js
+++ db.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const config = parse(process.env.DATABASE_URL);
+config.ssl = {
+  rejectUnauthorized: false
+};
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-});
-
+const pool = new Pool(config);
 export default pool;
